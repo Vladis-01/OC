608643c19f2e78f261a73e9e908564c24b237d7e
Влад Влад <vladis.a.-01@mail.ru>
2020-10-16 22:15:42 +0400

    ready

---
+++ Lab_1/OS_lab_1/src/com/SystemCall.java
@@ -0,0 +1,19 @@
+package com;
+
+import java.util.HashMap;
+
+public class SystemCall {
+    private int id;
+    private Object[] args;
+
+    public SystemCall(int id, Object[] args){
+        this.id = id;
+        this.args = args;
+    }
+    public Object[] getArgs(){
+        return args;
+    }
+    public int getId(){
+        return id;
+    }
+}
